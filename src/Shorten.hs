cd091d885a0a4e8c1130fac19252a6bead7c0540
Martino Visintin <vise890@gmail.com>
2015-02-02 17:43:45 -0500

    Update comments, remove dead ones

--- src/Shorten.hs
+++ src/Shorten.hs
@@ -8,7 +8,6 @@ import qualified Data.ByteString.Lazy.Char8 as LBS
 
 import qualified Bases as B
 
--- FIXME: I should really get a server up somewhere...
 baseUrl :: String
 baseUrl = "http://vise890.github.io/"
 
@@ -23,14 +22,13 @@ hash url = abs n
 
 toShortUnicode :: MD5HASH -> ShortUnicode
 toShortUnicode n = map toChar codePoints
-                   where n_unicode = (B.newNumber n 16) `B.toBase` 80 -- use less than the whole range for prettiness
+                   where n_unicode = (B.newNumber n 16) `B.toBase` 80 -- use less than the whole range for prettiness/emojiRange
                          codePoints = B.digits n_unicode
                          firstEmoji = fst . head $ readHex "1f600"
-                         toChar = Char.chr . (+firstEmoji) -- shift by 21 to avoid space and tab and such, this needs to be way more robust
+
+                         -- shift by at least 21 to avoid space and tab and such
+                         -- here, shifting to begin from the first emoji
+                         toChar = Char.chr . (+firstEmoji) 
 
 shorten :: URL -> URL
 shorten url = baseUrl ++ toShortUnicode (Shorten.hash url)
-
--- FIXME: be able to see type, instances of the type in the editor
--- FIXME: be able to search hoogle, from the editor
--- I shouldn't have to use Shorten.hash. bringing in MD5 as a qualified import should not interfere with my namespace.
